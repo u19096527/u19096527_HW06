3719d6061cc94b4acef689a22f1a2de0057c3670
Wisii-N <u19096527@tuks.co.za>
2022-10-30 04:01:14 +0200

    created and connected  DataModel to database

---
+++ Models/ViewModel/ProductVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u19096527_HW06.Models.ViewModel
+{
+    public class ProductVM
+    {
+        public
+        public 
+    }
+}
