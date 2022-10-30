4866e22f12d16888338fb8ad02a2bbc14eabeab0
Wisii-N <u19096527@tuks.co.za>
2022-10-30 23:16:08 +0200

    add modal Details

--- Controllers/productsController.cs
+++ Controllers/productsController.cs
@@ -103,8 +103,6 @@ namespace u19096527_HW06.Controllers
         }
 
         // POST: products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "product_id,product_name,brand_id,category_id,model_year,list_price")] product product)
